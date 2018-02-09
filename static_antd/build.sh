4c165a9931113c0cb0315c7e2e6c950b6a3358fc
舒达 <shuda.yy@alibaba-inc.com>
2018-02-09 15:25:27 +0800

    antDesign静态资源 与 普通静态资源相分离

--- static_antd/build.sh
+++ static_antd/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo 'npm build successfully, start moving files.'
-cp dist/index.html ../app/view/
-cp dist/* ../app/public/
+cp dist/index.html ../app/view/index.html
+rm -rf ../app/public/antDesign/*
+cp dist/* ../app/public/antDesign/
 echo 'Done.'
