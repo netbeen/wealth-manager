2f4a5296fa104b4889af2c10e3cc998b1599ca55
舒达 <shuda.yy@alibaba-inc.com>
2018-02-10 13:42:11 +0800

    升级ant design的npm包到最新

---
+++ app/public/antDesign/26.58c08eb2.async.js
@@ -0,0 +1,1 @@
+webpackJsonp([26],{1116:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var i=t(69),u=t.n(i),l=t(1),r=(t.n(l),t(97)),a=(t.n(r),t(317));n.default=function(){return u()(a.a,{type:"500",style:{minHeight:500,height:"80%"},linkElement:r.Link})}}});
--- app/public/antDesign/28.77f7a310.async.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([28],{1240:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var i=t(69),u=t.n(i),l=t(1),r=(t.n(l),t(97)),a=(t.n(r),t(318));n.default=function(){return u()(a.a,{type:"403",style:{minHeight:500,height:"80%"},linkElement:r.Link})}}});
