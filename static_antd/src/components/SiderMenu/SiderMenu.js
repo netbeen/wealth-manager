cb7579a099c31d11748b57498595efa9d0c3bfe8
舒达 <shuda.yy@alibaba-inc.com>
2018-02-11 21:48:44 +0800

    增加侧边栏菜单项

--- static_antd/src/components/SiderMenu/SiderMenu.js
+++ static_antd/src/components/SiderMenu/SiderMenu.js
@@ -221,7 +221,7 @@ export default class SiderMenu extends PureComponent {
         <div className={styles.logo} key="logo">
           <Link to="/">
             <img src={logo} alt="logo" />
-            <h1>游隼管家</h1>
+            <h1>Squirrel</h1>
           </Link>
         </div>
         <Menu
