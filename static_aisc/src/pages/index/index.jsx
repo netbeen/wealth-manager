ffc7e72a0076411f226cb0332ada32c50b93f79d
舒达 <shuda.yy@alibaba-inc.com>
2019-01-15 13:49:44 +0800

    enable eslint

--- static_aisc/src/pages/index/index.jsx
+++ static_aisc/src/pages/index/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import 'components/common/index.scss';
 import Root from './containers/root';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.scss';
 
-ReactDom.render(<Root />, document.getElementById('container'));
+ReactDom.render(
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/" component={Root} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+  , document.getElementById('container')
+);
