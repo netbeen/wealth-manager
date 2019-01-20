0ec4111666717aac6bd5c0187a703ac49812266a
舒达 <shuda.yy@alibaba-inc.com>
2019-01-20 17:41:11 +0800

    enable hsts in prod env

--- config/config.prod.js
+++ config/config.prod.js
@@ -5,5 +5,15 @@ module.exports = () => {
 
   config.sequelize = secretConfig
 
+  config.security = {
+    hsts: {
+      enable: true,
+    },
+    csrf: {
+      enable: false, // 暂时关闭csrf
+      ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+    },
+  };
+
   return config;
 };
