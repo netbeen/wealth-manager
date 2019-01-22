00472f95d2f9d0e2cb337761f127617cea1672ff
舒达 <shuda.yy@alibaba-inc.com>
2019-01-22 16:03:46 +0800

    update style

--- static_aisc/src/pages/index/routePages/error404.jsx
+++ static_aisc/src/pages/index/routePages/error404.jsx
@@ -13,7 +13,7 @@ class Login extends Component {
     return (
       <div className="page-error-wrap" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', width: '100vw', height: '100vh' }}>
         <img width="400" height="400" alt="404" src="https://img.alicdn.com/tfs/TB1HFKaD1ySBuNjy1zdXXXPxFXa-700-700.png" />
-        <div style={{ fontSize: 36 }}>404 - 找不到对应的页面</div>
+        <div style={{ fontSize: 36, color: '#969ca8' }}>404 - 找不到对应的页面</div>
       </div>
     );
   }
