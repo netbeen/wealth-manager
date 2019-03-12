636f7655bd8a449de43c899b22b20970b5b58201
舒达 <shuda.yy@alibaba-inc.com>
2019-03-12 20:10:48 +0800

    支持输出千分位格式化的货币

--- static_aisc/src/components/formattedCurrency/index.jsx
+++ static_aisc/src/components/formattedCurrency/index.jsx
@@ -7,6 +7,22 @@ export const CURRENCY_COLOR = {
   WEALTH: Symbol('WEALTH'),
 };
 
+const toThousands = (numString) => {
+  if (!numString || !parseFloat(numString)) {
+    return null;
+  }
+  const [, sign, integer, decimal] = numString.match(/(-)?(.*)\.(.*)?$/);
+  let integerResult = '';
+  let integerRemain = integer;
+
+  while (integerRemain.length > 3) {
+    integerResult = `,${integerRemain.slice(-3)}${integerResult}`;
+    integerRemain = integerRemain.slice(0, integerRemain.length - 3);
+  }
+  if (integerRemain) { integerResult = integerRemain + integerResult; }
+  return `${sign === '-' ? '-' : ''}${integerResult}.${decimal}`;
+};
+
 export default ({
   value = 0,
   color = CURRENCY_COLOR.NONE,
@@ -22,12 +38,18 @@ export default ({
   } else if (color === CURRENCY_COLOR.WEALTH) {
     //
   }
+  let displayValue = value;
+  if (percentage) {
+    displayValue = (displayValue * 100).toFixed(2);
+  } else {
+    displayValue = toThousands(displayValue.toFixed(2));
+  }
   return (
     <span
       className="formatted-currency"
       style={style}
     >
-      {(percentage ? value * 100 : value).toFixed(2)}
+      {displayValue}
       {percentage && '%'}
     </span>
   );
