fdeb843adae84855cf99ffab9eb02f2635bfd388
舒达 <shuda.yy@alibaba-inc.com>
2020-01-19 14:12:19 +0800

    增加lrp计算器，开发中

---
+++ lpr_analization/main.js
@@ -0,0 +1,52 @@
+console.log('loanBaseRate1Y',window.loanBaseRate1Y);
+const dom = document.getElementById('container');
+const myChart = echarts.init(dom);
+const app = {};
+option = null;
+option = {
+  title: {
+    text: '折线图堆叠',
+  },
+  tooltip: {
+    trigger: 'axis',
+  },
+  legend: {
+    data: ['贷款基准利率1Y', '贷款基准利率5Y'],
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+    },
+  },
+  xAxis: {
+    type: 'time',
+    boundaryGap: false,
+    // data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      name: '贷款基准利率1Y',
+      type: 'line',
+      smooth: true,
+      data: window.loanBaseRate1Y,
+    }, {
+      name: '贷款基准利率5Y',
+      type: 'line',
+      smooth: true,
+      data: window.loanBaseRate5Y,
+    },
+  ],
+};
+
+if (option && typeof option === 'object') {
+  myChart.setOption(option, true);
+}
